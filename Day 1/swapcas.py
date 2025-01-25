fdd4f3eceacbe2cbc76f3ec53b35061891bf6a7d
Abinesh <abineshpcm@gmail.com>
2025-01-25 10:55:45 +0530

    s

--- Day 1/swapcas.py
+++ Day 1/swapcas.py
@@ -1,3 +1,5 @@
+"""You are given a string and your task is to swap cases. 
+In other words, convert all lowercase letters to uppercase letters and vice versa."""
 def swapcase(s):
     result = ""
     for char in s:
